cedafe3bf69b9721c6085306e180c63a16c2afcf
nicksolony <nicksolony@gmail.com>
2023-10-16 17:11:55 -0400

    installed redux persist and setup store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'reduxjs-toolkit-persist';
+import storage from 'reduxjs-toolkit-persist/lib/storage';
 import { contactsReducer } from "./contacts/contactsSlice";
 import { filterReducer } from "./filter/filterSlice";
 
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
 export const store = configureStore({
     reducer: {
-        contacts: contactsReducer,
+        contacts: persistReducer(persistConfig, contactsReducer),
         filter: filterReducer,
     }
 });
+
+export const persistor = persistStore(store);
 
